7c25d3cf86ee52ae23144ff5ade327bc6f5742f1
jt146476 <54554424+jt146476@users.noreply.github.com>
2019-11-14 10:22:26 -0600

    Initial commit

---
+++ SimpleCalculator/SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace SimpleCalculator
+{
+    partial class Calc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_results = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.txt_entry1 = new System.Windows.Forms.TextBox();
+            this.txt_entry2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_results
+            // 
+            this.btn_results.Location = new System.Drawing.Point(82, 295);
+            this.btn_results.Name = "btn_results";
+            this.btn_results.Size = new System.Drawing.Size(113, 68);
+            this.btn_results.TabIndex = 0;
+            this.btn_results.Text = "Click me to get results ";
+            this.btn_results.UseVisualStyleBackColor = true;
+            this.btn_results.Click += new System.EventHandler(this.btn_results_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter first number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter second number ";
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.BackColor = System.Drawing.Color.Cornsilk;
+            this.lbl_result.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_result.Location = new System.Drawing.Point(274, 305);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(100, 23);
+            this.lbl_result.TabIndex = 3;
+            // 
+            // txt_entry1
+            // 
+            this.txt_entry1.Location = new System.Drawing.Point(294, 61);
+            this.txt_entry1.Name = "txt_entry1";
+            this.txt_entry1.Size = new System.Drawing.Size(100, 20);
+            this.txt_entry1.TabIndex = 4;
+            // 
+            // txt_entry2
+            // 
+            this.txt_entry2.Location = new System.Drawing.Point(294, 151);
+            this.txt_entry2.Name = "txt_entry2";
+            this.txt_entry2.Size = new System.Drawing.Size(100, 20);
+            this.txt_entry2.TabIndex = 5;
+            // 
+            // Calc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 450);
+            this.Controls.Add(this.txt_entry2);
+            this.Controls.Add(this.txt_entry1);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_results);
+            this.Name = "Calc";
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_results;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_result;
+        private System.Windows.Forms.TextBox txt_entry1;
+        private System.Windows.Forms.TextBox txt_entry2;
+    }
+}
+
---
+++ SimpleCalculator/SimpleCalculator/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Calc : Form
+    {
+        public Calc()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_results_Click(object sender, EventArgs e)
+        {
+            int num1;
+            int num2;
+
+            int sum;
+
+            num1 = Convert.ToInt32(txt_entry1.Text);
+            num2 = Convert.ToInt32(txt_entry2.Text);
+
+            sum = num1 + num2;
+
+            lbl_result.Text = "Sum is " + sum; 
+        }
+    }
+}
